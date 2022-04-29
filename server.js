584324178d698ba3492ccf2b3a2b87c4334bab37
eslamamged <solomamged72@gmail.com>
2022-04-29 07:51:11 +0200

    edit

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const port = process.env.PORT;
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+
 mongoose
-  .connect(DB, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
